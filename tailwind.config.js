46a3c97e21f35df1af2b466c141ca1d274c1ce74
Tommi Salomaa <tommi.salomaa@gmail.com>
2023-03-28 01:37:52 +0300

    Changed bk image from png to jpg

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'tommi-bk' : "url('/assets/tommisalomaa-bk.png')",
+        'tommi-bk' : "url('/assets/tommisalomaa-bk.jpg')",
       },
       boxShadow: {
         '3xl': '0 35px 60px -15px rgba(255, 255, 255, 0.3)',
