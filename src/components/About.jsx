974a6e70379faf733f64b716fb8b2b05e6bc6606
Tommi Salomaa <tommi.salomaa@gmail.com>
2023-04-05 09:41:12 +0300

    Update to About section.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -38,6 +38,14 @@ const About = () => {
                                 has a large network of top talent in music, games and media
                             </li>
                         </ul>
+                        <div>
+                        <h3 className='pt-8 border-b'>What others have to say?</h3>
+                            <p className='italic  text-rose-300'>
+                                "Tommi provided the perfect musical supplement to our gaming themed event and handled both DJ and live playing duties with great skill.
+                                Highly recommended."
+                            </p>
+                            <p className='text-sm'>&emsp;-Samuli Syvähuoko, serial entrepreneur, founding member of several game studios including Remedy</p>
+                        </div>
 
                 </div>
             </div>
